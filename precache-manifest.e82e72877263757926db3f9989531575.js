98067f3fddf25fa9421d8ffd6b178a2e826680c9
Jeremy Parmenter <shabado9889@gmail.com>
2019-08-11 04:32:09 -0700

    Updates

---
+++ precache-manifest.e82e72877263757926db3f9989531575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bebb62e464c5170877013ead3d5d5f5",
+    "url": "/guess-pokemon/index.html"
+  },
+  {
+    "revision": "f448c3e35dbf28919e8c",
+    "url": "/guess-pokemon/static/css/main.6317f8e9.chunk.css"
+  },
+  {
+    "revision": "6da6949eb84ecd671844",
+    "url": "/guess-pokemon/static/js/2.827207b6.chunk.js"
+  },
+  {
+    "revision": "f448c3e35dbf28919e8c",
+    "url": "/guess-pokemon/static/js/main.8971e4ff.chunk.js"
+  },
+  {
+    "revision": "afc622e285f70465451c",
+    "url": "/guess-pokemon/static/js/runtime~main.9ca3975f.js"
+  }
+]);
